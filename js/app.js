b1f6203bd89ff083dc1f5b5b9c30ec1b1132f10f
chanmyaeoo <chanmyaeoo1996mgk@gmail.com>
2020-02-16 21:04:27 +0630

    finished links data adding

--- js/app.js
+++ js/app.js
@@ -1,10 +1,72 @@
 const addLink = document.querySelector('#add-link');
 const modalBg = document.querySelector('.modal-bg');
 const modalClose = document.querySelector('.modal-close');
+const titleInput = document.querySelector('#title');
+const despInput = document.querySelector('#description');
+const linkInput = document.querySelector('#link');
+const category = document.querySelector('#category');
+const addLinkForm = document.querySelector('.modal__form');
+const cardWrap = document.querySelector('.card-wrap');
+
+let linksData = [];
+
+// Handle modal open and close
 addLink.addEventListener('click', e => {
   modalBg.classList.add('modal-bg-active');
 });
 
 modalClose.addEventListener('click', e => {
   modalBg.classList.remove('modal-bg-active');
+});
+
+// Handle add Link process
+addLinkForm.addEventListener('submit', e => {
+  e.preventDefault();
+  let title = e.target.elements.title.value;
+  let description = e.target.elements.description.value;
+  let link = e.target.elements.link.value;
+  let linkCategory = category.options[category.selectedIndex].value;
+  const markUp = `
+      <div class="card">
+        <div class="card__top">
+          <span>Share</span>
+          <span>Fav</span>
+        </div>
+
+        <div class="card__content">
+          <div class="card__title">
+            <a href="${link}" target="_blank">${title}</a>
+          </div>
+          <div class="card__description">
+            <p>
+              ${description.length === 0 ? '---' : description}
+            </p>
+          </div>
+        </div>
+
+        <div class="card__bottom">
+          <i class="far fa-edit"></i>
+          <i class="far fa-trash-alt"></i>
+        </div>
+      </div>
+    </div>
+  `;
+
+  // Check inputs to push data int linksData
+  if (title.length > 0 && link.length > 0) {
+    linksData.push({
+      title,
+      description,
+      link,
+      linkCategory
+    });
+    e.target.elements.title.value = '';
+    e.target.elements.description.value = '';
+    e.target.elements.link.value = '';
+    modalBg.classList.remove('modal-bg-active');
+
+    cardWrap.insertAdjacentHTML('afterbegin', markUp);
+  }
+
+  console.log(linksData);
 });
